6ff224da92e16525ae3c53d23eaf9c40be7f03f9
helloaltop <helloaltop@gmail.com>
2022-02-22 21:30:17 +0700

    update

---
+++ 5_variable.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama = "Muhammad Ali Topan"
+	fmt.Println(nama)
+}
---
+++ 7_type_declaration.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	type married bool
+
+	var isMarried married = true
+
+	fmt.Println(isMarried)
+}
